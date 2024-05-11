094b8904988601ff1eadc4a4dc7839560a0620e7
ika <mamina747@gmail.com>
2024-05-11 22:09:15 +0300

    test

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -8,6 +8,7 @@ def test_get_date():
 
 def test_mask_from_to():
     assert mask_from_to("Счет 64686473678894779589") == "Счет **9589"
+    assert mask_from_to("Счет 38976430693692818358") == "Счет **8358"
 
 
 def test_mask_from_to_one():
@@ -15,14 +16,30 @@ def test_mask_from_to_one():
 
 
 def test_get_filtered_and_sorted():
-    if "state" == "EXECUTED":
-        assert get_filtered_and_sorted == ['26.08.2019 Перевод организации\n'
-                                           'Maestro 1596 83** **** 5199 Счет ****9589\n'
-                                           '8221.37 USD\n']
+    test_list = [{"state": "EXECUTED"},
+                 {"date": "2019-08-26T10:50:58.294041"}]
+    result = [{"state": "EXECUTED"},
+              {"date": "2019-08-26T10:50:58.294041"}]
 
+    assert get_filtered_and_sorted(test_list) == get_filtered_and_sorted(result)
 
 def test_prepare_user_msg():
-    if "state" == "EXECUTED":
-        assert prepare_user_msg == ["26.08.2019 Перевод организации\n"
-                                    "Maestro 1596 83** **** 5199 -> Счет ****9589\n"
-                                    "8221.37 USD\n"]
+    test_list = {"id": 41428829,
+                 "state": "EXECUTED",
+                 "date": "2019-07-03T18:35:29.512364",
+                 "operationAmount": {
+                     "amount": "8221.37",
+                     "currency": {
+                         "name": "USD",
+                         "code": "USD"
+                     }
+                 },
+                 "description": "Перевод организации",
+                 "from": "MasterCard 7158300734726758",
+                 "to": "Счет 35383033474447895560"
+                 }
+    test_msg = """03.07.2019 Перевод организации
+MasterCard 7158 30** **** 6758 -> Счет **5560
+8221.37 USD"""
+
+    assert prepare_user_msg(test_list) == test_msg
